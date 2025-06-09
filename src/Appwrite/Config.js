da8c8cfb30ddba9f4420593b5949ffe5e8d347f4
mheshd <delluser946@gmail.com>
2025-06-09 12:18:26 +0545

    fix the config

--- src/Appwrite/Config.js
+++ src/Appwrite/Config.js
@@ -3,10 +3,10 @@ const config = {
   appWriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
   appWriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
   appWriteUserCollectionId: String(
-    import.meta.env.VITE_APPWRITE_USER_COLLECTIONID_ID
+    import.meta.env.VITE_APPWRITE_USER_COLLECTION_ID
   ),
   appWriteAppointmentsCollectionId: String(
-    import.meta.env.VITE_APPWRITE_APPOINTMENTS_ID
+    import.meta.env.VITE_APPWRITE_APPOINTEMENT_ID
   ),
   appWriteAvailableSlots: String(import.meta.env.VITE_APPWRITE_AVIABLESLOT_ID),
 };
